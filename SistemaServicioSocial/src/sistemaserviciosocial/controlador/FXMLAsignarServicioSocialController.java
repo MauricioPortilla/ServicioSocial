fc09c2606a1dccddf18684458777a6bb3fcdcd61
Mauricio Cruz Portilla <mauricioportilla189@hotmail.com>
2019-05-28 09:28:04 -0500

    Avance Asignacion SS 2

--- SistemaServicioSocial/src/sistemaserviciosocial/controlador/FXMLAsignarServicioSocialController.java
+++ SistemaServicioSocial/src/sistemaserviciosocial/controlador/FXMLAsignarServicioSocialController.java
@@ -6,11 +6,16 @@ import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import sistemaserviciosocial.Alumno;
 import sistemaserviciosocial.AlumnoDAO;
@@ -90,7 +95,27 @@ public class FXMLAsignarServicioSocialController {
 
 		alumnosTableView.setItems(alumnoDAO.getAlumnos(true));
 		solicitudesTableView.setItems(solicitudDAO.getSolicitudes());
-		
+
+		asignarButton.setOnAction(asignarButtonHandler());
+	}
+
+	private EventHandler<ActionEvent> asignarButtonHandler() {
+		return new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				if (alumnosTableView.getSelectionModel().getSelectedItems().size() < 1) {
+					new Alert(AlertType.WARNING, "Debes seleccionar un alumno de la tabla").show();
+					return;
+				} else if (solicitudesTableView.getSelectionModel().getSelectedItems().size() < 1) {
+					new Alert(AlertType.WARNING, "Debes seleccionar una solicitud de la tabla").show();
+					return;
+				}
+				Alert asignacionAlert = new Alert(AlertType.CONFIRMATION, "¿Desea realizar esta asignación?");
+				if (asignacionAlert.showAndWait().get() == ButtonType.OK) {
+					System.out.println("Asignando");
+				}
+			}
+		};
 	}
 
 }
