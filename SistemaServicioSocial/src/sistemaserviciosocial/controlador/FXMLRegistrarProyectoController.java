7876c460859149c501775ced062b21b9b0d0f3fc
Bruno Luján <bruno1529@live.com.mx>
2019-06-08 00:56:30 -0500

    Terminé la interfáz del CU Registrar proyecto y modifiqué el controlador

--- SistemaServicioSocial/src/sistemaserviciosocial/controlador/FXMLRegistrarProyectoController.java
+++ SistemaServicioSocial/src/sistemaserviciosocial/controlador/FXMLRegistrarProyectoController.java
@@ -13,21 +13,113 @@ package sistemaserviciosocial.controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import sistemaserviciosocial.Alumno;
+import sistemaserviciosocial.Solicitud;
 
 /**
- * FXML Controller class
- *
- * @author Bruno
+ * FXMLRegistrarProyectoController es la clase que lleva el control de la interfaz
+ * FXMLRegistrarProyecto, la cual registra un proyecto a partir de una solicitud no asignada.
+ * 
+ * @author Bruno Antonio López Luján 
+ * @version 1.0
+ * @since 2019/06/07
  */
-public class FXMLRegistrarProyectoController implements Initializable {
+public class FXMLRegistrarProyectoController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TableColumn<Solicitud, String> actividadesTableColumn;
+
+    @FXML
+    private TableView<Alumno> alumnosAsignadosTableView;
+
+    @FXML
+    private Button cancelarButton;
+
+    @FXML
+    private TextField descripcionTextField;
+
+    @FXML
+    private TextField fechaInicioTextField;
+
+    @FXML
+    private TableColumn<Solicitud, String> horarioTableColumn;
+
+    @FXML
+    private TextField horarioTextField;
+
+    @FXML
+    private TableColumn<Solicitud, String> lugarTableColumn;
+
+    @FXML
+    private TableColumn<Alumno, String> maternoTableColumn;
+
+    @FXML
+    private TableColumn<Alumno, String> matriculaTableColumn;
+
+    @FXML
+    private TableColumn<Solicitud, String> noAlumnosTableColumn;
+
+    @FXML
+    private TableColumn<Alumno, String> nombreTableColumn;
+
+    @FXML
+    private TextField nombreTextField;
+
+    @FXML
+    private TableColumn<Alumno, String> paternoTableColumn;
+
+    @FXML
+    private Button registrarButton;
+
+    @FXML
+    private TableColumn<Solicitud, String> requisitosTableColumn;
+
+    @FXML
+    private TableColumn<Solicitud, String> responsableTableColumn;
+
+    @FXML
+    private TableView<Solicitud> solicitudesTableView;
+
+    @FXML
+    private TableColumn<Solicitud, String> unidadTableColumn;
 
   /**
    * Initializes the controller class.
    */
-  @Override
-  public void initialize(URL url, ResourceBundle rb) {
-    // TODO
-  }  
-  
+    @FXML
+    void initialize() {
+        assert actividadesTableColumn != null : "fx:id=\"actividadesTableColumn\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert alumnosAsignadosTableView != null : "fx:id=\"alumnosAsignadosTableView\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert cancelarButton != null : "fx:id=\"cancelarButton\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert descripcionTextField != null : "fx:id=\"descripcionTextField\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert fechaInicioTextField != null : "fx:id=\"fechaInicioTextField\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert horarioTableColumn != null : "fx:id=\"horarioTableColumn\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert horarioTextField != null : "fx:id=\"horarioTextField\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert lugarTableColumn != null : "fx:id=\"lugarTableColumn\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert maternoTableColumn != null : "fx:id=\"maternoTableColumn\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert matriculaTableColumn != null : "fx:id=\"matriculaTableColumn\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert noAlumnosTableColumn != null : "fx:id=\"noAlumnosTableColumn\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert nombreTableColumn != null : "fx:id=\"nombreTableColumn\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert nombreTextField != null : "fx:id=\"nombreTextField\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert paternoTableColumn != null : "fx:id=\"paternoTableColumn\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert registrarButton != null : "fx:id=\"registrarButton\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert requisitosTableColumn != null : "fx:id=\"requisitosTableColumn\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert responsableTableColumn != null : "fx:id=\"responsableTableColumn\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert solicitudesTableView != null : "fx:id=\"solicitudesTableView\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+        assert unidadTableColumn != null : "fx:id=\"unidadTableColumn\" was not injected: check your FXML file 'FXMLRegistrarProyecto.fxml'.";
+
+
+    }
+
 }
